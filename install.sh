aa938e155519e87b17800d28fb77ddb13d0348d7
Pun <54141879+pun341@users.noreply.github.com>
2025-11-03 10:34:13 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo mkdir /nauos
+sudo mv ./parse.lua ./nauos
+sudo apt install luajit
+sudo touch /usr/bin/rebuild
+
+sudo chmod +x /usr/bin/rebuild
+
+sudo echo "luajit /nauos/parse.lua" > /usr/bin/rebuild
+
+sudo chmod +x /usr/bin/rebuild
